5109ab6669d8e75e3cafd98ef3b21d69f28cd12a
Alex Babis <acbabis@gmail.com>
2017-10-14 02:13:34 -0700

    Mission track

--- src/game/CommitteeHistoryPanel.js
+++ src/game/CommitteeHistoryPanel.js
@@ -2,16 +2,33 @@ import React, { Component } from 'react';
 
 export default class CommitteeHistoryPanel extends Component {
   render() {
-    const {missionHistory} = this.props.game;
+    const {state, missionHistory, missionSizes} = this.props.game;
     return (
       <div className="history">
         {
-          missionHistory.map(({numFails, isFailure}, index) =>
-            <div key={index}
-              className={isFailure ? 'failure' : 'success'}>
-              {numFails} fail votes
-            </div>
-          )
+          new Array(5).fill(null).map((u, index) => {
+              const mission = missionHistory[index];
+              const isMissionActive = state === 'mission' && index === missionHistory.length;
+              if(mission) {
+              }
+              return <div key={index}
+                className="mission"
+                data-active={isMissionActive}
+                data-completed={!!mission}
+              >
+                <div class="front">
+                  <span>{missionSizes[index]}</span>
+                </div>
+                <div class="back">
+                  {(() => {
+                    if(mission) {
+                      const {isFailure} = mission;
+                      return <span>{isFailure ? '❌' : '✔️'}</span>
+                    }
+                  })()}
+                </div>
+              </div>
+          })
         }
       </div>
     );
