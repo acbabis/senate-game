07e93b70b7074e01d03a5fac7ebc3bc86e14df19
Alex Babis <acbabis@gmail.com>
2017-10-14 05:02:54 -0700

    Good guys can't choose to fail on a committee

--- src/game/CommitteePanel.js
+++ src/game/CommitteePanel.js
@@ -3,14 +3,19 @@ import React, { Component } from 'react';
 export default class CommitteePanel extends Component {
   render() {
     const {game, connection} = this.props;
-    const {playerIndex, currentMissionGroup, hasMadeSelection} = game;
+    const {playerIndex, badFaction, currentMissionGroup, hasMadeSelection} = game;
     const isPlayerOnMission = currentMissionGroup.includes(playerIndex);
     if(isPlayerOnMission && !hasMadeSelection) {
       return <div className="committee"> 
-        <button className="action succeed" onClick={() => connection.sendMissionAction(true)}>
+        <button
+          className="action succeed"
+          onClick={() => connection.sendMissionAction(true)}>
           Succeed
         </button>
-        <button className="action fail" onClick={() => connection.sendMissionAction(false)}>
+        <button
+          className="action fail"
+          disabled={!badFaction}
+          onClick={() => connection.sendMissionAction(false)}>
           Fail
         </button>
       </div>
