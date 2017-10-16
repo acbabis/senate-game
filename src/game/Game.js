b1681e17533a19a2d24b023677c0747974f58bde
Alex Babis <acbabis@gmail.com>
2017-10-16 04:45:01 -0700

    Committee outcome messages

--- src/game/Game.js
+++ src/game/Game.js
@@ -8,9 +8,32 @@ import RolePanel from './RolePanel';
 import VotePanel from './VotePanel';
 
 export default class Game extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showOutcome: false
+    };
+  }
+
+  componentWillReceiveProps({game: newGame}) {
+    const {game: oldGame} = this.props;
+    if(newGame.state === 'nomination' && oldGame.state === 'mission') {
+      // Mission just ended
+      this.setState({
+        showOutcome: true
+      });
+    } else if(newGame.state === 'vote' && this.state.showOutcome) {
+      // Automatically close if game proceeds
+      this.setState({
+        showOutcome: false
+      });
+    }
+  }
+
   render() {
+    const {showOutcome} = this.state;
     const {connection, game} = this.props;
-    const {state} = game
+    const {state, missionHistory} = game
     return (
       <div className="game">
         <div className="top">
@@ -31,6 +54,31 @@ export default class Game extends Component {
           })()}
           <RolePanel game={game} />
         </div>
+        {(() => {
+          if(!showOutcome) {
+            return null;
+          } else {
+            const {numFails, isFailure} = missionHistory.slice(-1)[0];
+            return <div
+              className="committee-outcome"
+              data-failure={isFailure}
+              onClick={() => this.setState({showOutcome: false})}
+            >
+              <span>{(() => {
+                if(!isFailure && numFails > 0) {
+                  return 'Committee succeeded with 1 nay vote';
+                } else if(!isFailure) {
+                  return 'Committee succeeded unanimously';
+                } else if(numFails == null) { // Auto-fail from deadlock
+                  return 'Failed to form a committee';
+                } else {
+                  return `Committee failled with ${numFails} nay vote${numFails > 1 ? 's' : ''}`;
+                }
+              })()}
+              </span>
+            </div>
+          }
+        })()}
       </div>
     );
   }
