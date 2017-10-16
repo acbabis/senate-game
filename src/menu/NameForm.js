41ce36930ed10670728c2457728fa60f5b86da04
Alex Babis <acbabis@gmail.com>
2017-10-15 20:21:25 -0700

    Autofill name

--- src/menu/NameForm.js
+++ src/menu/NameForm.js
@@ -4,7 +4,7 @@ export default class NameForm extends Component {
   constructor() {
     super();
     this.state = {
-      username: ''
+      lastUsername: localStorage.username || ''
     };
   }
 
@@ -13,6 +13,7 @@ export default class NameForm extends Component {
     const {username} = this.state;
     const {connection, onDone} = this.props;
     if(username) {
+      localStorage.lastUsername  = username;
       connection.setUsername(username);
     }
     if(onDone) {
@@ -22,10 +23,13 @@ export default class NameForm extends Component {
 
   componentDidMount() {
     this.input.focus();
+    this.input.select();
   }
 
   render() {
+    const {lastUsername} = this.state;
     const {username} = this.props;
+
     return (
       <div className="enter-name">
         <form className="menu-container username" onSubmit={(e) => this.sendUsername(e)}>
@@ -34,6 +38,7 @@ export default class NameForm extends Component {
             id="username"
             name="username"
             type="text"
+            value={lastUsername || ''}
             placeholder={username}
             onChange={({target}) => this.setState({username: target.value})}
             ref={input => this.input = input} />
