cc9404c0e1539f7dbc76f97c012dd0728834b3ce
Alex Babis <acbabis@gmail.com>
2017-10-17 13:15:57 -0700

    Lint fixes

--- src/index.js
+++ src/index.js
@@ -22,11 +22,10 @@ if (!Object.entries) {
 }
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
-if (typeof Object.assign != 'function') {
+if (typeof Object.assign !== 'function') {
     // Must be writable: true, enumerable: false, configurable: true
     Object.defineProperty(Object, "assign", {
         value: function assign(target, varArgs) { // .length of function is 2
-            'use strict';
             if (target == null) { // TypeError if undefined or null
                 throw new TypeError('Cannot convert undefined or null to object');
             }
